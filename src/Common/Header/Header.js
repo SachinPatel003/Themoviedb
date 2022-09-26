4f053c5aaad9f797642ddf14aecba2042cd1f167
Sachin <sachinpatel460@gmail.com>
2022-09-26 17:24:53 +0530

    test Commit

--- src/Common/Header/Header.js
+++ src/Common/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
       {/* </Link> */}
       <ul className="items">
         <li className="item">
-          <Link to="/">Movies</Link>
+          <Link to="/">Movies Test</Link>
         </li>
         <li className="item">
           <Link to="/TVShows">TV Shows</Link>
